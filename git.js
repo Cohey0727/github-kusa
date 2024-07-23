db6e25eeb5fd7d4f94aad6faab946ef268e2f76d
KoheiOkamoto <ohayousagi.ac.kook0727@gmail.com>
2024-07-23 19:14:01 +1000

    main

---
+++ git.js
@@ -0,0 +1,28 @@
+const { exec } = require("child_process");
+
+const main = async () => {
+  let index = 0;
+  // 2017年1月1日
+  const baseDate = 1483272000000;
+  for (const a of [...Array(183).keys()]) {
+    await exec2(`echo ${index} > index.txt`);
+    await exec2("git add .");
+    const date = new Date(baseDate + index * 1000 * 60 * 60 * 24 * 2);
+    await exec2(`git commit -m 'add ' --date="${date.toISOString()}"`);
+    index++;
+  }
+  await exec2(`git push`);
+};
+
+const exec2 = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+};
+main();
